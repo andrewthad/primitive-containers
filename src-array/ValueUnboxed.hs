d87ec917eb89388d634512553d4650786f0e99a2
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2018-05-09 18:55:55 +0200

    Replace Ctx datatype by Ctx type family to allow for empty contexts

--- src-array/ValueUnboxed.hs
+++ src-array/ValueUnboxed.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 
 {-# OPTIONS_GHC -O2 #-}
 
@@ -10,35 +11,36 @@ import Data.Primitive.PrimArray
 
 type MArr = MutablePrimArray
 type Arr = PrimArray
-type Ctx = Prim
+type family Ctx a where
+  Ctx a = Prim a
 
-new :: Prim a => Int -> ST s (MutablePrimArray s a)
+new :: Ctx a => Int -> ST s (MutablePrimArray s a)
 new = newPrimArray
 
-index :: Prim a => PrimArray a -> Int -> a
+index :: Ctx a => PrimArray a -> Int -> a
 index = indexPrimArray
 
-read :: Prim a => MutablePrimArray s a -> Int -> ST s a
+read :: Ctx a => MutablePrimArray s a -> Int -> ST s a
 read = readPrimArray
 
-write :: Prim a => MutablePrimArray s a -> Int -> a -> ST s ()
+write :: Ctx a => MutablePrimArray s a -> Int -> a -> ST s ()
 write = writePrimArray
 
-resize :: Prim a => MutablePrimArray s a -> Int -> ST s (MutablePrimArray s a)
+resize :: Ctx a => MutablePrimArray s a -> Int -> ST s (MutablePrimArray s a)
 resize = resizeMutablePrimArray
 
-copy :: Prim a => MutablePrimArray s a -> Int -> PrimArray a -> Int -> Int -> ST s ()
+copy :: Ctx a => MutablePrimArray s a -> Int -> PrimArray a -> Int -> Int -> ST s ()
 copy = copyPrimArray
 
 unsafeFreeze :: MutablePrimArray s a -> ST s (PrimArray a)
 unsafeFreeze = unsafeFreezePrimArray
 
-size :: Prim a => PrimArray a -> Int
+size :: Ctx a => PrimArray a -> Int
 size = sizeofPrimArray
 
-foldr :: Prim a => (a -> b -> b) -> b -> PrimArray a -> b
+foldr :: Ctx a => (a -> b -> b) -> b -> PrimArray a -> b
 foldr = foldrPrimArray
 
-map :: (Prim a, Prim b) => (a -> b) -> PrimArray a -> PrimArray b
+map :: (Ctx a, Ctx b) => (a -> b) -> PrimArray a -> PrimArray b
 map = mapPrimArray
 
