d0c3f96af1cf74d98c5363e92798c1f673ce238d
Andrew Martin <andrew.thaddeus@gmail.com>
2018-07-19 20:44:35 -0400

    add unlifted subset

--- src/Data/Set/Unlifted.hs
+++ src/Data/Set/Unlifted.hs
@@ -6,7 +6,7 @@
 
 {-# OPTIONS_GHC -O2 #-}
 module Data.Set.Unlifted
-  ( Set
+  ( S.Set
   , empty
   , singleton
   , member
@@ -14,8 +14,8 @@ module Data.Set.Unlifted
   , difference
     -- * Conversion
   , toArray
-  , toList
-  , fromList
+  , S.toList
+  , S.fromList
     -- * Folds
   , foldr
   , foldMap
@@ -30,50 +30,12 @@ import Prelude hiding (foldr,foldMap)
 
 import Data.Primitive.UnliftedArray (UnliftedArray, PrimUnlifted(..))
 import Data.Semigroup (Semigroup)
+import Data.Set.Unlifted.Internal (Set(..))
 import qualified Data.Foldable as F
 import qualified Data.Semigroup as SG
 import qualified GHC.Exts as E
 import qualified Data.Set.Internal as I
-
--- | A set of elements.
-newtype Set a = Set (I.Set UnliftedArray a)
-
-instance PrimUnlifted (Set a) where
-  toArrayArray# (Set x) = toArrayArray# x
-  fromArrayArray# y = Set (fromArrayArray# y)
-
-instance (PrimUnlifted a, Ord a) => Semigroup (Set a) where
-  Set x <> Set y = Set (I.append x y)
-  stimes = SG.stimesIdempotentMonoid
-  sconcat xs = Set (I.concat (E.coerce (F.toList xs)))
-
-instance (PrimUnlifted a, Ord a) => Monoid (Set a) where
-  mempty = Set I.empty
-  mappend = (SG.<>)
-  mconcat xs = Set (I.concat (E.coerce xs))
-
-instance (PrimUnlifted a, Eq a) => Eq (Set a) where
-  Set x == Set y = I.equals x y
-
-instance (PrimUnlifted a, Ord a) => Ord (Set a) where
-  compare (Set x) (Set y) = I.compare x y
-
--- | The functions that convert a list to a 'Set' are asymptotically
--- better that using @'foldMap' 'singleton'@, with a cost of /O(n*log n)/
--- rather than /O(n^2)/. If the input list is sorted, even if duplicate
--- elements are present, the algorithm further improves to /O(n)/. The
--- fastest option available is calling 'fromListN' on a presorted list
--- and passing the correct size size of the resulting 'Set'. However, even
--- if an incorrect size is given to this function,
--- it will still correctly convert the list into a 'Set'.
-instance (PrimUnlifted a, Ord a) => E.IsList (Set a) where
-  type Item (Set a) = a
-  fromListN n = Set . I.fromListN n
-  fromList = Set . I.fromList
-  toList (Set s) = I.toList s
-
-instance (PrimUnlifted a, Show a) => Show (Set a) where
-  showsPrec p (Set s) = I.showsPrec p s
+import qualified Data.Set.Unlifted.Internal as S
 
 -- | Test for membership in the set.
 member :: (PrimUnlifted a, Ord a) => a -> Set a -> Bool
@@ -99,14 +61,6 @@ difference (Set x) (Set y) = Set (I.difference x y)
 -- order. This function is zero-cost.
 toArray :: Set a -> UnliftedArray a
 toArray (Set s) = I.toArray s
-
--- | /O(n)/ Convert a set to a list. The elements are given in ascending order.
-toList :: PrimUnlifted a => Set a -> [a]
-toList (Set s) = I.toList s
-
--- | /O(n*log n)/ Convert a list to a set.
-fromList :: (PrimUnlifted a, Ord a) => [a] -> Set a
-fromList = Set . I.fromList
 
 -- | Right fold over the elements in the set. This is lazy in the accumulator.
 foldr :: PrimUnlifted a
